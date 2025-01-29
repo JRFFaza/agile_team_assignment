38ea98f9ba1eb95bf0e43dbb544baf4fb4da60f6
jpnas <jp.nassar8@gmail.com>
2025-01-29 14:58:25 +0200

    Minor changes + code for image tracking

---
+++ Assets/Scripts/ImageTracker.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+using Unity.VisualScripting;
+
+public class ImageTracker : MonoBehaviour
+{
+    private ARTrackedImageManager trackedImages;
+    public GameObject[] ArPrefabs;
+
+    List<GameObject> ARObjects = new List<GameObject>();
+
+
+    private void Awake()
+    {
+        trackedImages = GetComponent<ARTrackedImageManager>();
+    }
+
+    private void OnEnable()
+    {
+        trackedImages.trackedImagesChanged += OnTrackedImagesChanged;
+    }
+
+    private void OnDisable()
+    {
+        trackedImages.trackedImagesChanged -= OnTrackedImagesChanged;
+    }
+
+    private void OnTrackedImagesChanged(ARTrackedImagesChangedEventArgs eventArgs)
+    {
+        foreach (var trackedImage in eventArgs.added)
+        {
+            foreach (var arPrefab in ArPrefabs)
+            {
+                if (trackedImage.referenceImage.name == arPrefab.name)
+                {
+                    var newPrefab = Instantiate(arPrefab, trackedImage.transform);
+                    ARObjects.Add(newPrefab);
+                }
+            }
+        }
+
+        foreach (var trackedImage in eventArgs.updated)
+        {
+            foreach (var gameObject in ARObjects)
+            {
+                if (gameObject.name == trackedImage.name)
+                {
+                    gameObject.SetActive(trackedImage.trackingState == TrackingState.Tracking);
+                }
+            }
+        }
+    }
+
+
+}
