ab6963a875469e4f9bff927b7e7743dd6520eedd
KharkivskaK <148970104+KharkivskaK@users.noreply.github.com>
2025-02-03 14:55:42 +0200

    InfoButton is edited

--- Assets/Scripts/InfoButton.cs
+++ Assets/Scripts/InfoButton.cs
@@ -3,6 +3,18 @@ using UnityEngine.SceneManagement;
 
 public class InfoButton : MonoBehaviour
 {
+    /* // Start is called once before the first execution of Update after the MonoBehaviour is created
+     void Start()
+     {
+
+     }
+
+     // Update is called once per frame
+     void Update()
+     {
+
+     }*/
+
     public void OpenInfo()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
