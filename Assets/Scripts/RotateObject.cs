6636853971a91a4c0540cedb2c3e9053969a2fa6
JRFFaza <JRFFaza@uclan.ac.uk>
2025-02-08 19:51:18 +0200

    add rotateobject script - not working, and try to fix rotation with lean touch

---
+++ Assets/Scripts/RotateObject.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class RotateObjectController : MonoBehaviour
+{
+    public float PCRotationSpeed = 10f;
+    public float MobileRotationSpeed = 0.4f;
+    //Drag the camera object here
+    public Camera cam;
+
+    void Update()
+    {
+        // get the user touch input
+        foreach (Touch touch in Input.touches)
+        {
+            Debug.Log("Touching at: " + touch.position);
+            Ray camRay = cam.ScreenPointToRay(touch.position);
+            RaycastHit raycastHit;
+            if (Physics.Raycast(camRay, out raycastHit, 10))
+            {
+                if (touch.phase == TouchPhase.Began)
+                {
+                    Debug.Log("Touch phase began at: " + touch.position);
+                }
+                else if (touch.phase == TouchPhase.Moved)
+                {
+                    Debug.Log("Touch phase Moved");
+                    transform.Rotate(touch.deltaPosition.y * MobileRotationSpeed,
+                        -touch.deltaPosition.x * MobileRotationSpeed, 0, Space.World);
+                }
+                else if (touch.phase == TouchPhase.Ended)
+                {
+                    Debug.Log("Touch phase Ended");
+                }
+            }
+        }
+    }
+}
