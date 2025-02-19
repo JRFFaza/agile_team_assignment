31fad6ed2ae270fb8ec7aeeba15f7e6fffa0d3a1
jpnas <jp.nassar8@gmail.com>
2025-02-19 14:42:01 +0200

    added cube with floating panels

--- Assets/Scripts/StartButton.cs
+++ Assets/Scripts/StartButton.cs
@@ -19,6 +19,7 @@ public class StartButton : MonoBehaviour
     {
         //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         //SceneManager.LoadScene("ARScene");
-        SceneManager.LoadScene("QRJoyce");
+        //SceneManager.LoadScene("QRJoyce");
+        SceneManager.LoadScene("Test");
     }
 }
