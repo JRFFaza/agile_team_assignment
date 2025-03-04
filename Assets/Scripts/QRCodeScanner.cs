7328a178234bde5b3a4baf303ce347056bd01052
JRFFaza <JRFFaza@uclan.ac.uk>
2025-03-04 15:30:19 +0200

    added permissions

--- Assets/Scripts/QRCodeScanner.cs
+++ Assets/Scripts/QRCodeScanner.cs
@@ -27,6 +27,11 @@ public class QRCodeScanner : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        if (!Permission.HasUserAuthorizedPermission(Permission.Camera))
+        {
+            Permission.RequestUserPermission(Permission.Camera);
+        }
+
         SetUpCamera();
     }
 
