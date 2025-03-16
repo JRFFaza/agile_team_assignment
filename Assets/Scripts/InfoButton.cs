1b53e71751d43e88f258b99fb9316c7eb8b9525b
jpnas <jp.nassar8@gmail.com>
2025-03-16 12:57:17 +0200

    add floor maps for all scenes

--- Assets/Scripts/InfoButton.cs
+++ Assets/Scripts/InfoButton.cs
@@ -17,7 +17,7 @@ public class InfoButton : MonoBehaviour
 
     public void OpenInfo()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 3);
-        //SceneManager.LoadScene("InfoScreen");
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 3);
+        SceneManager.LoadScene("InfoScreen");
     }
 }
