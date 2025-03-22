4631b7965fc20e917a9c42a72d7d5204da81c759
JRFFaza <JRFFaza@uclan.ac.uk>
2025-03-22 15:55:27 +0200

    fix info scene button

--- Assets/Scripts/InfoButton.cs
+++ Assets/Scripts/InfoButton.cs
@@ -6,6 +6,6 @@ public class InfoButton : MonoBehaviour
     public void OpenInfo()
     {
         //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 3);
-        SceneManager.LoadScene("InfoScreen");
+        SceneManager.LoadScene("InfoScene");
     }
 }
